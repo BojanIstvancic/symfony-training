75ffaec0310798c2dc7d606ff65ffa906a746d33
BojanIstvancic <bojan.istvancic@gmail.com>
2022-11-20 21:06:12 +0100

    add displayAllMovies route, add displaySingleMovie route

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,41 +2,33 @@
 
 namespace App\Controller;
 
-use App\Entity\Movie;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    private $em;
+    private $moviesRepository;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(MovieRepository $repository)
     {
-        $this->em = $em;
+        $this->moviesRepository = $repository;
     }
 
-    #[Route('/movies', name: 'movies')]
-
+    #[Route('/movies',  methods: ['GET'],  name: 'movies')]
     public function index(): Response
     {
-        // BuiltInQuerie
-        // findAll() -> SELECT * FROM 'movies'
-        // find(1) -> SELECT * FROM 'movies' WHERE id=1
-        // findBy([], ['id' => 'DESC']) -> SELECT * FROM 'movies' ORDER BY id DESC
-        // findOneBy(['id' => 7, 'title' => 'The Dark Knight'], ["id" => 'DESC']); 
-        // SELECT * FROM 'movies' WHERE id = 6 AND title = 'The Dark Knight' ORDER BY id DESC
-        // count([]) -> count amount of elements in table
-        // count(["id" => 7] -> SELECT COUNT() from movies WHERE  id = 1
-        // getClassName(); -> get EntityName
+        return $this->render('/movies/index.html.twig', [
+            'movies' => $this->moviesRepository->findAll()
+        ]);
+    }
 
-        // we can also create custom queries
-
-        $repository = $this->em->getRepository(Movie::class);
-        $movies = $repository->getClassName();
-        // dd($movies); // var_dump for symfony
-
-        return $this->render('/movies/index.html.twig');
+    #[Route('/movies/{id}',  methods: ['GET'],  name: 'movie')]
+    public function show($id): Response
+    {
+        return $this->render('/movies/show.html.twig', [
+            'movie' => $this->moviesRepository->find($id)
+        ]);
     }
 }
